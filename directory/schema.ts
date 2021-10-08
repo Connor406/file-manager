17b21f9d02fb411d993246f5c59959bbed432efc
Connor Mahan <mahan406@gmail.com>
2021-10-08 11:12:20 -0500

    last crud ops on directory

--- directory/schema.ts
+++ directory/schema.ts
@@ -1,3 +1,4 @@
+import { Directory } from ".prisma/client"
 import { createModule, gql } from "graphql-modules"
 import { prismaClient } from "../prisma"
 import * as directoryService from "./service"
@@ -19,10 +20,13 @@ export const directoryModule = createModule({
 
       extend type Query {
         getAllDirectories: [Directory]!
+        getDirectory(id: ID!): Directory
       }
 
       type Mutation {
         createDirectory(name: String!, parentId: String!): Directory!
+        renameDirectory(id: ID!, name: String!): Directory!
+        deleteDirectory(id: ID!): Boolean!
       }
     `,
   ],
@@ -31,13 +35,28 @@ export const directoryModule = createModule({
       getAllDirectories: () => {
         return prismaClient().directory.findMany()
       },
+      getDirectory: async (
+        _: unknown,
+        { id }: { id: Directory["id"] }
+      ): Promise<Directory | null> => {
+        return await directoryService.getDirectory(prismaClient(), id)
+      },
     },
     Mutation: {
       createDirectory: async (
         _: unknown,
         { name, parentId }: { name: string; parentId: string }
-      ) => {
+      ): Promise<Directory> => {
         return await directoryService.createDirectory(prismaClient(), name, parentId)
+      },
+      renameDirectory: async (
+        _: unknown,
+        { id, name }: { id: Directory["id"]; name: Directory["name"] }
+      ): Promise<Directory> => {
+        return await directoryService.renameDirectory(prismaClient(), id, name)
+      },
+      deleteDirectory: async (_: unknown, { id }: { id: Directory["id"] }): Promise<boolean> => {
+        return await directoryService.deleteDirectory(prismaClient(), id)
       },
     },
   },
