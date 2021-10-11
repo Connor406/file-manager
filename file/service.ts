1dca5b822e868f530af9f457e69fdc4ee33df463
Connor Mahan <mahan406@gmail.com>
2021-10-11 10:03:19 -0500

    update `history` column on file creation

--- file/service.ts
+++ file/service.ts
@@ -12,6 +12,33 @@ export type CreateFileInput = Prisma.FileGetPayload<typeof fileInputFields> &
     key?: FileVersion["key"]
   }
 
+export async function updateFileHistory(
+  client: PrismaClient,
+  id: File["id"],
+  entry: Record<string, string | number | boolean>
+): Promise<Prisma.JsonArray> {
+  const file = await client.file.findUnique({ where: { id }, select: { history: true } })
+
+  if (!file) {
+    throw new Error("File not found")
+  }
+
+  const history =
+    file.history && typeof file.history === "object" && Array.isArray(file.history)
+      ? file.history
+      : []
+
+  const updatedHistory = [
+    ...history,
+    {
+      ...entry,
+      date: new Date().toString(),
+    },
+  ]
+
+  return updatedHistory
+}
+
 export async function createFileRecord(
   client: PrismaClient,
   file: CreateFileInput
@@ -25,6 +52,15 @@ export async function createFileRecord(
     name,
     directoryId,
     ancestors: [...ancestors, directoryId],
+    history: [
+      {
+        action: "created",
+        name,
+        mimeType,
+        size,
+        directoryId,
+      },
+    ],
     versions: {
       create: {
         name,
